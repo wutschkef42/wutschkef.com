f0aa923b5bb6aca733fc28e965830dd85bcd01f3
wutschkef42 <fwutschk@student.42.fr>
2018-06-07 19:52:57 -0700

    churning out progress

--- wutschkef-com/routes/index.js
+++ wutschkef-com/routes/index.js
@@ -38,6 +38,7 @@ exports = module.exports = function (app) {
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.all('/contact', routes.views.contact);
+	app.all('/now', routes.views.now);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
--- wutschkef-com/routes/middleware.js
+++ wutschkef-com/routes/middleware.js
@@ -21,6 +21,7 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Blog', key: 'blog', href: '/blog' },
+		{ label: 'Now', key: 'now', href: '/now'},
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
